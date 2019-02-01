052f8de3fa7a3dad0194c1c07996131a58722cee
AMarinov <amarinov@infragistics.com>
2019-02-01 09:26:47 +0200

    Replacing else ifs with a switch clause

--- Step 4/CityService/Controllers/CitiesController.cs
+++ Step 4/CityService/Controllers/CitiesController.cs
@@ -28,31 +28,29 @@ namespace CityService.Controllers
 
 			foreach (var transaction in transactions)
 			{
-				if (transaction.type == "update")
+				var city = db.Cities.Where(x => x.CityID == transaction.id).FirstOrDefault();
+				switch (transaction.type)
 				{
+					case "update":					
 
-					// Find the city that needs to be updated
-					var city = db.Cities.Where(x => x.CityID == transaction.id).FirstOrDefault();
+						// Update the properties values
+						db.Entry(city).CurrentValues.SetValues(transaction.newValue);
 
-					// Update the properties values
-					db.Entry(city).CurrentValues.SetValues(transaction.newValue);
+						// You can set the values property by property instead of using SetValues()
 
-					// You can set the values property by property instead of using SetValues()
+						//city.CityName = transaction.newValue.CityName;
+						//city.Population = transaction.newValue.Population;
+						//city.TrainStation = transaction.newValue.TrainStation;
+						//city.HolidayDate = transaction.newValue.HolidayDate;
+						//city.Description = transaction.newValue.Description;
+						break;
+					case "add":
+						db.Cities.Add(transaction.newValue);
+						break;
+					case "delete":
+						db.Cities.Remove(city);
+						break;
 
-					//city.CityName = transaction.newValue.CityName;
-					//city.Population = transaction.newValue.Population;
-					//city.TrainStation = transaction.newValue.TrainStation;
-					//city.HolidayDate = transaction.newValue.HolidayDate;
-					//city.Description = transaction.newValue.Description;
-				}
-				else if (transaction.type == "add")
-				{
-					db.Cities.Add(transaction.newValue);
-				}
-				else if (transaction.type == "delete")
-				{
-					var city = db.Cities.Where(x => x.CityID == transaction.id).FirstOrDefault();
-					db.Cities.Remove(city);
 				}
 			}
 			try
