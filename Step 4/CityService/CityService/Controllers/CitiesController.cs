17d82497f0d01c0adad01ce0043c3d2c0782e6f0
AMarinov <amarinov@infragistics.com>
2019-02-07 19:20:37 +0200

    Updating the controller

--- Step 4/CityService/CityService/Controllers/CitiesController.cs
+++ Step 4/CityService/CityService/Controllers/CitiesController.cs
@@ -1,133 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Web.Http.Description;
 using CityService.Models;
 
 namespace CityService.Controllers
 {
-	[Route("api/Cities")]
 	[EnableCors(origins: CORSConfig.allowedOrigins, headers: CORSConfig.allowedHeaders, methods: CORSConfig.allowedMethods, SupportsCredentials = true)]
-	public class CitiesController : ApiController
+	public class Cities1Controller : ApiController
     {
-		private CityServiceContext db = new CityServiceContext();
-		// GET: api/Cities
-		public IList<City> GetCities()
-		{
-			return db.Cities.ToList();
+        private CityServiceContext db = new CityServiceContext();
+
+        // GET: api/Cities1
+        public IQueryable<City> GetCities()
+        {
+            return db.Cities;
+        }
+
+        // GET: api/Cities1/5
+        [ResponseType(typeof(City))]
+        public async Task<IHttpActionResult> GetCity(int id)
+        {
+            City city = await db.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
+        }
+
+        // PUT: api/Cities1/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCity(City[] cities)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+			foreach (var city in cities)
+			{
+				db.Entry(city).State = EntityState.Modified;
+			}
+
+            await db.SaveChangesAsync();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Cities1
+        [ResponseType(typeof(City))]
+        public async Task<IHttpActionResult> PostCity(City[] cities)
+        {
+			Dictionary<int, City> results = new Dictionary<int, City>();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+			foreach (var city in cities)
+			{
+				results.Add(city.CityID, city);
+				db.Cities.Add(city);
+			}
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", null, results);
+        }
+
+        // DELETE: api/Cities1/5
+        [ResponseType(typeof(City))]
+        public async Task<IHttpActionResult> DeleteCity([FromUri] int[] ids)
+        {
+			foreach (var id in ids)
+			{
+				City city = await db.Cities.FindAsync(id);
+				if (city == null)
+				{
+					return NotFound();
+				}
+				db.Cities.Remove(city);
+			}
+            await db.SaveChangesAsync();
+
+			return StatusCode(HttpStatusCode.NoContent);
 		}
 
-		[HttpDelete]
-		[Route("api/Cities/Delete")]
-		[EnableCors(origins: CORSConfig.allowedOrigins, headers: CORSConfig.allowedHeaders, methods: CORSConfig.allowedMethods, SupportsCredentials = true)]
-		public IHttpActionResult DeleteCity(int id)
-		{
-			var city = db.Cities.Where(x => x.CityID == id).FirstOrDefault();
-			db.Cities.Remove(city);
-			try
-			{
-				db.SaveChanges();
-			}
-			catch (Exception e)
-			{
-				return BadRequest(e.Message);
-			}
-			return Ok();
-		}
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		[HttpPost]
-		[Route("api/Cities/Add")]
-		[EnableCors(origins: CORSConfig.allowedOrigins, headers: CORSConfig.allowedHeaders, methods: CORSConfig.allowedMethods, SupportsCredentials = true)]
-		public IHttpActionResult AddCity(ITransaction<City> transaction)
-		{
-			db.Cities.Add(transaction.newValue);
-			try
-			{
-				db.SaveChanges();
-			}
-			catch (Exception e)
-			{
-				return BadRequest(e.Message);
-			}
-			return Ok();
-		}
-
-		[HttpPut]
-		[Route("api/Cities/Update")]
-		[EnableCors(origins: CORSConfig.allowedOrigins, headers: CORSConfig.allowedHeaders, methods: CORSConfig.allowedMethods, SupportsCredentials = true)]
-		public IHttpActionResult UpdateCity(ITransaction<City> transaction)
-		{
-			var city = db.Cities.Where(x => x.CityID == transaction.id).FirstOrDefault();
-			db.Entry(city).CurrentValues.SetValues(transaction.newValue);
-			try
-			{
-				db.SaveChanges();
-			}
-			catch (Exception e)
-			{
-				return BadRequest(e.Message);
-			}
-			return Ok();
-		}
-
-
-		// POST: api/Cities/UpdateCities
-		//public IHttpActionResult UpdateCities(ITransaction<City>[] transactions)
-		//{
-
-		//	foreach (var transaction in transactions)
-		//	{
-		//		var city = db.Cities.Where(x => x.CityID == transaction.id).FirstOrDefault();
-		//		switch (transaction.type)
-		//		{
-		//			case "update":
-
-		//				// Update the properties values
-		//				db.Entry(city).CurrentValues.SetValues(transaction.newValue);
-
-		//				// You can set the values property by property instead of using SetValues()
-
-		//				//city.CityName = transaction.newValue.CityName;
-		//				//city.Population = transaction.newValue.Population;
-		//				//city.TrainStation = transaction.newValue.TrainStation;
-		//				//city.HolidayDate = transaction.newValue.HolidayDate;
-		//				//city.Description = transaction.newValue.Description;
-		//				break;
-		//			case "add":
-		//				db.Cities.Add(transaction.newValue);
-		//				break;
-		//			case "delete":
-		//				db.Cities.Remove(city);
-		//				break;
-
-		//		}
-		//	}
-		//	try
-		//	{
-		//		db.SaveChanges();
-		//	}
-		//	catch (Exception e)
-		//	{
-		//		return BadRequest(e.Message);
-		//	}
-		//	return Ok();
-		//}
-
-		public class ITransaction<T>
-		{
-			public int id;
-			public T newValue;
-			public string type;
-		}
-
-	}
-
-	public static class CORSConfig
-	{
-		public const string allowedOrigins = "http://localhost:4200";
-		public const string allowedHeaders = "*";
-		public const string allowedMethods = "*";
-	}
+        private bool CityExists(int id)
+        {
+            return db.Cities.Count(e => e.CityID == id) > 0;
+        }
+    }
 }
